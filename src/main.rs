9a103e2730432b451006f4ba45c8852f895dfccc
PawanSikawat <pawansikawat1998@gmail.com>
2024-01-01 19:54:18 +0530

    inititating terminal and polling for key events with exit on esc or q

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::error::Error;
+use std::{error::Error, io, time::Duration};
+use crossterm::{terminal::{self, LeaveAlternateScreen, EnterAlternateScreen}, ExecutableCommand, cursor::{Hide, Show}, event::{self, Event, KeyCode}};
 use invaders::add_audios;
 use rusty_audio::Audio;
 
@@ -9,7 +10,33 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Starting up
     audio.play("startup");
 
+    // Initialize Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Game Loop
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') | KeyCode::Char('Q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+
     // Cleanup post game
     audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
     Ok(())
 }
